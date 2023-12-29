1820effc6397109c59b95925bea992fc4656e004
Nandhini Kandasamy <97506671+Nandhinikandasamy1@users.noreply.github.com>
2023-12-29 21:49:54 +0530

    Update timestamp.py

--- timestamp.py
+++ timestamp.py
@@ -1,19 +1,23 @@
-def get_time_stamp():
-    getcurrenttime = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    return getcurrenttime
+def get_time_stamp(): #Creating Function to get the curent time
+    getcurrenttime = datetime.datetime.now().strftime("%Y%m%d-%H%M%S") #getting time and storing that in a variable
+    return getcurrenttime # Returning the time
 
-def write_to_file(ts):
+def write_to_file(ts): # creating function to write the name of the text file
     timestampfile = open(ts + ".txt", "w")
     timestampfile.close()
     with open(ts + ".txt", "a", newline='') as file_write:
         writer = csv.writer(file_write)
         writer.writerow([ts])
 
-def read_from_file(filename):
+
+#Question number 2:
+def read_from_file(filename): #Creating a function to read the input of the file
     with open(filename + ".txt", 'r') as file_read:
         filecontent = file_read.readlines()
     return filecontent
 
+
+#Program to call all the functions and get the desired output
 import datetime
 import csv
 time_stamp = get_time_stamp()
